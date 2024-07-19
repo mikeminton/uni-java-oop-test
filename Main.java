78bed8efb16667ad59f0bdbee83d581e876bac9d
Mike Minton <mikeminton.uk@outlook.com>
2024-07-19 19:01:18 +0100

    Extended out put to three lines not one line

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println("Hey Motha Fuckers!");
+      System.out.println("I am learning Java.");
+      System.out.println("It might be awesome!");
     }
   }
