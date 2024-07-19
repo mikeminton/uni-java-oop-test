bd686c822a28c8eb8267a2238ad94583c25508bc
Mike Minton <mikeminton.uk@outlook.com>
2024-07-19 19:08:46 +0100

    Added multiple lines and comments to the code file

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+      // This is a comment
       System.out.println("Hey Motha Fuckers!");
-      System.out.println("I am learning Java.");
-      System.out.println("It might be awesome!");
+      System.out.print("I am learning Java.");
+      System.out.print(" It might be awesome!");
+      System.out.println(); 
+      System.out.println(3);
+      /* The code below will print the words and numbers
+      to the screen, and it is amazing */
+      System.out.println(358);
+      System.out.println(50000);
+      System.out.println(3 + 3);
+      System.out.println(2 * 5);  // This is also a comment
+
+
     }
   }
