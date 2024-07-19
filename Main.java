3a4f8f4bd87fdeef69bad8308b515a6dc83660d9
Mike Minton <mikeminton.uk@outlook.com>
2024-07-19 19:20:07 +0100

    Added multiple variable with assignments and quirkly little tests

--- Main.java
+++ Main.java
@@ -13,6 +13,45 @@ public class Main {
       System.out.println(3 + 3);
       System.out.println(2 * 5);  // This is also a comment
 
+      String name = "John";
+      System.out.println(name);
+
+      int myNum = 15;
+      System.out.println(myNum);
+
+      int myNum0;
+      myNum0= 17;
+      System.out.println(myNum0);
+
+      int myNum1 = 15;
+      myNum1 = 20;  // myNum is now 20
+      System.out.println(myNum1);
+      int     myNum2     = 5;
+      float   myFloatNum = 5.99f;
+      char    myLetter   = 'D';
+      boolean myBool     = true;
+      String  myText     = "Hello";
+      // compiler error above as the variables are not used
+      
+      String firstName = "John ";
+      String lastName  = "Doe";
+      String fullName  = firstName + lastName;
+      System.out.println(fullName);
+
+      int x = 5;
+      int y = 6;
+      int z = 50;
+      System.out.println(x + y + z);
+
+      int xa = 5, ya = 6, za = 50;
+      System.out.println(xa + ya + za);
+
+      int xb, yb, zb;
+      xb = yb = zb = 50;
+      System.out.println(xb + yb + zb);
+
+
+
 
     }
   }
