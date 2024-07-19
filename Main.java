2128b45a11ccfcf8b5e32879bd469e95359acc9a
Mike Minton <mikeminton.uk@outlook.com>
2024-07-19 21:35:32 +0100

    Messed around with data types

--- Main.java
+++ Main.java
@@ -50,6 +50,40 @@ public class Main {
       xb = yb = zb = 50;
       System.out.println(xb + yb + zb);
 
+      // Good
+      int minutesPerHour = 60;
+
+      // OK, but not so easy to understand what m actually is
+      int m = 60;
+
+      // Student data
+      String studentName = "John Doe";
+      int studentID = 15;
+      int studentAge = 23;
+      float studentFee = 75.25f;
+      char studentGrade = 'B';
+
+      // Print variables
+      System.out.println("Student name: " + studentName);
+      System.out.println("Student id: " + studentID);
+      System.out.println("Student age: " + studentAge);
+      System.out.println("Student fee: " + studentFee);
+      System.out.println("Student grade: " + studentGrade);
+
+        // Create integer variables
+      int length = 4;
+      int width  = 6;
+      int area;
+
+      // Calculate the area of a rectangle
+      area = length * width;
+
+      // Print variables
+      System.out.println("Length is: " + length);
+      System.out.println("Width is: " + width);
+      System.out.println("Area of the rectangle is: " + area);
+
+      
 
 
 
