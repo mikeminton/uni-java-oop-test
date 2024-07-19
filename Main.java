09473514b508a4651923595b27d33b2c3087020c
Mike Minton <mikeminton.uk@outlook.com>
2024-07-19 21:43:49 +0100

    Added boolean, chars and working examples.

--- Main.java
+++ Main.java
@@ -90,6 +90,39 @@ public class Main {
       double myNum12 = 19.99d;
       System.out.println(myNum12);
 
+      float  f1 = 35e3f;
+      double d1 = 12E4d;
+      System.out.println(f1);
+      System.out.println(d1);
+        //#endregion#
+      
+
+      boolean isJavaFun   = true;
+      boolean isFishTasty = false;
+      System.out.println(isJavaFun);    // Outputs true
+      System.out.println(isFishTasty);  // Outputs false
+
+      char myGrade = 'B';
+      System.out.println(myGrade);
+
+      char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+      System.out.println(myVar1);
+      System.out.println(myVar2);
+      System.out.println(myVar3);
+
+      String greeting = "Hello World";
+      System.out.println(greeting);
+
+      // Create variables of different data types
+      int items = 50;
+      float costPerItem = 9.99f;
+      float totalCost = items * costPerItem;
+      char currency = '$';
+
+      // Print variables
+      System.out.println("Number of items: " + items);
+      System.out.println("Cost per item: " + costPerItem + currency);
+      System.out.println("Total cost = " + totalCost + currency);
 
     }
   }
